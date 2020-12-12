19b8238cb1a3c4ee5e139e020b858bc5d68c703d
Marek Pokropiński <marek.pokropinski@outlook.com>
2020-12-12 15:53:03 +0100

    add vizualization for fetched data

--- twitter_data/fetch_data.py
+++ twitter_data/fetch_data.py
@@ -3,10 +3,11 @@ import twint
 import os
 
 c = twint.Config()
-c.Search = "#polska OR #polityka lang:pl"
+# c.Search = "#polska OR #polityka lang:pl"
+c.Search = "#polityka lang:pl"
 c.Since = '2019-11-16'
 c.Store_csv = True
 c.Tabs = True
 c.Hide_output = True
-c.Output = os.path.join('twitter_data', 'data', 'data.csv')
+c.Output = os.path.join('twitter_data', 'data', 'polityka.csv')
 twint.run.Search(c)
